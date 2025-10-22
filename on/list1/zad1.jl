6bea534721857d996ec94928691720c8fc7ea79f
zofia <ztkal2013@gmail.com>
2025-10-23 01:28:29 +0200

    done z1

--- on/list1/zad1.jl
+++ on/list1/zad1.jl
@@ -3,6 +3,8 @@
 
 # one() - zwraca jedynkę w danym typie danych
 
+types = [Float16, Float32, Float64]
+
 function macheps(type)
     eps = one(type)     # jedyna w danym typie
     while one(type) + eps / type(2) > one(type)
@@ -13,8 +15,7 @@ end
 
 println("------Liczba eps------")
 
-
-for t in [Float16, Float32, Float64]
+for t in types
     # t to typ danych
     println(t)
     println(rpad("iteracyjnie: ", 15), macheps(t))
@@ -34,11 +35,11 @@ end
 
 println("------Liczba eta------")
 
-for t in [Float16, Float32, Float64]
+for t in types
     # t to typ danych
     println(t)
     println(rpad("iteracyjnie: ", 15), find_eta(t))
-    println(rpad("funkcja eps: ", 15), nextfloat(t(0.0)), '\n')
+    println(rpad("funkcja nextfloat: ", 15), nextfloat(t(0.0)), '\n')
 end
 
 println("------Wartości precyzji------")
@@ -57,7 +58,26 @@ println("------Badanie floatmin------")
 println("Float16: ", floatmin(Float16))
 println("Float32: ", floatmin(Float32))
 println("Float64: ", floatmin(Float64))
+
 println("------Badanie MIN_nor------")
 println("Float16: ", Float16(2^-14))
 println("Float32: ", Float32(2^-126))
 println("Float64: ", Float64(2^-1022))
+
+
+
+function find_max(type)
+    num = prevfloat(type(1.0))
+    while !isinf(2*num)
+        num *= 2
+    end
+    return num
+end
+
+println("------MAX------")
+
+for t in types
+    println(t)
+    println(rpad("iteracyjnie: ", 15), find_max(t))
+    println(rpad("funkcja floatmax: ", 15), floatmax(t), '\n')
+end
