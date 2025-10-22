8500abea88513d2d65ba99079c1f7e69c19107ea
zofia <ztkal2013@gmail.com>
2025-10-23 00:10:51 +0200

    przerwa

---
+++ on/list1/zad1.jl
@@ -0,0 +1,63 @@
+
+# Zofia Tarchalska
+
+# one() - zwraca jedynkę w danym typie danych
+
+function macheps(type)
+    eps = one(type)     # jedyna w danym typie
+    while one(type) + eps / type(2) > one(type)
+        eps /= type(2)
+    end
+    return eps
+end
+
+println("------Liczba eps------")
+
+
+for t in [Float16, Float32, Float64]
+    # t to typ danych
+    println(t)
+    println(rpad("iteracyjnie: ", 15), macheps(t))
+    println(rpad("funkcja eps: ", 15), eps(t), '\n')
+end
+
+
+
+function find_eta(type)
+    eta = one(type)
+    while type(0.5) * eta > zero(type)
+        eta *= type(0.5)
+    end
+    return eta
+end
+
+
+println("------Liczba eta------")
+
+for t in [Float16, Float32, Float64]
+    # t to typ danych
+    println(t)
+    println(rpad("iteracyjnie: ", 15), find_eta(t))
+    println(rpad("funkcja eps: ", 15), nextfloat(t(0.0)), '\n')
+end
+
+println("------Wartości precyzji------")
+
+println("Float16: ", Float16(2^-10))
+println("Float32: ", Float32(2^-23))
+println("Float64: ", Float64(2^-52))
+
+println("------Wartości MIN_sub------")
+println("Float16: ", Float16(2^-24))
+println("Float32: ", Float32(2^-149))
+println("Float64: ", Float64(2^-1074))
+
+
+println("------Badanie floatmin------")
+println("Float16: ", floatmin(Float16))
+println("Float32: ", floatmin(Float32))
+println("Float64: ", floatmin(Float64))
+println("------Badanie MIN_nor------")
+println("Float16: ", Float16(2^-14))
+println("Float32: ", Float32(2^-126))
+println("Float64: ", Float64(2^-1022))
