2b91e20d5e2e280fb1dc5188bf556e6d8b53fd98
zofia <ztkal2013@gmail.com>
2025-12-02 23:58:00 +0100

    z1-z4 almost done, to do tests

---
+++ on/list4/interpolation.jl
@@ -0,0 +1,77 @@
+module Interpolation
+export ilorazyRoznicowe, warNewton, naturalna, rysujNnfx
+using Plots
+
+function ilorazyRoznicowe(x::Vector{Float64}, f::Vector{Float64})
+
+    x_len = length(x)
+    fx = zeros(Float64, x_len)
+    for i in 1:x_len
+        fx[i] = f[i]
+    end
+    for j in 2:x_len
+        for i in n:-1:j
+            fx[i] = (fx[i] - fx[i-1])/(x[i]-x[i-j+1])
+        end
+    end
+    return fx
+    
+end
+
+function warNewton(x::Vector{Float64}, fx::Vector{Float64}, t::Float64)
+    n = length(x) - 1
+
+    w = fx[n + 1]
+    for i in n:-1:1
+        w = w * (t - x[i])  + fx[i]  
+    end
+    return w
+end
+
+function naturalna(x::Vector{Float64}, fx::Vector{Float64})
+    n = length(x) - 1
+    a = zeros(Float64, n + 1)
+
+    a[n + 1] = fx[n + 1]
+    for k in n:-1:1 
+        for j in k:-1:1
+            a[j] = a[j] - x[k] * a[j + 1]         
+        end 
+        a[k] = a[k] + fx[k]        
+    end
+end
+
+
+function rysujNnfx(f,a::Float64,b::Float64,n::Int)
+
+    x = zeros(n+1)
+    y = zeros(n+1)
+    h = (b-a)/n
+
+    for k in 1:(n+1)
+        x[k] = a + k*h
+        y[k] = f(x[k])
+    end
+
+    c = ilorazyRoznicowe(x, y)
+
+    points = 50 * (n+1)         # siatka punktów (50 razy gęściej niż liczba węzłów)
+    dx = (b-a)/(points-1)
+    xs = zeros(points)          # punkty w przedziale [a, b]
+    poly = zeros(points)        # wartości przyjmowane przez wielomian w tych punktach
+    func = zeros(points)        # wartości przyjmowane przez funckję w tych punktach
+    xs[1] = a
+    poly[1] = func[1] = y[1]
+
+    for i in 2:points
+        xs[i] = xs[i-1] + dx
+        poly[i] = warNewton(x, c, xs[i])
+        func[i] = f(xs[i])
+    end
+    p = plot(xs, [poly func], label=["wielomian" "funkcja"], title="n = $n")
+    display(p)
+    return p
+end
+
+
+end
